d21274d71d2f63340f195e9e81e2a578a5f10d62
Mohanraja <mohanrajak1411@gmail.com>
2026-07-26 14:29:48 +0530

    Point frontend to live backend URL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://resume-builder-backend-bjwk.onrender.com/api',
 })
 
 api.interceptors.request.use((config) => {
